427d7081d3030387fe1e8cbda10e0873b0be7e7f
a4alaniz <a4alaniz@gmail.com>
2019-12-06 14:06:00 -0600

    linkcard

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,7 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/candidates">Presidential Candidates</NavLink>
+      <NavLink to="/">Presidential Candidates</NavLink>
       <NavLink to="/politicians">Your Politicians</NavLink>
     </div>
   );
